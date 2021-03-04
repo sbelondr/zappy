5665365f47df5eac7e636237c4ce89ba0033b752
potato <superpotager@gmail.com>
2021-03-04 15:54:36 +0100

    parser V1

--- server.c
+++ server.c
@@ -11,7 +11,7 @@
 int BindCreatedSocket(int hSocket)
 {
 	int iRetval=-1;
-	int ClientPort = 90191;
+	int ClientPort = 8080;
 	struct sockaddr_in  remote= {0};
 	/* Internet address family */
 	remote.sin_family = AF_INET;
@@ -24,36 +24,57 @@ int BindCreatedSocket(int hSocket)
 
 int main(void)
 {
+	int	sockets[10];
 	int	socket_desc;
 	int	sock;
 	char client_message[200];
 	size_t	clientLen;
 	char	map[10][10] = {0};
+	char	buffer[200];
 	int		playerx = 5;
 	int		playery = 5;
 	struct sockaddr_in client;
 	fd_set fd;
 
+	soc
 	clientLen = sizeof(struct sockaddr_in);
 	socket_desc = socket(AF_INET, SOCK_STREAM, 0);
 	BindCreatedSocket(socket_desc);
 	listen(socket_desc, 3);
-	sock = accept(socket_desc,(struct sockaddr *)&client,(socklen_t*)&clientLen);
 	printf("Accepted client\n");
+	FD_ZERO(&fd);
+	FD_SET(socket_desc, &fd);
+	struct timeval timeout;
+	timeout.tv_sec = 10;
+	timeout.tv_usec = 0;
+	if (select(sock + 1, &fd, NULL, &fd, &timeout))
+	{
+	printf("accepted\n");
+	sock = accept(socket_desc,(struct sockaddr *)&client,(socklen_t*)&clientLen);
+	printf("accepted\n");
+	}
+	else
+	{
+		printf("Timeoueted\n");
+		exit(1);
+	}
 	while (1)
 	{
 		struct timeval timeout;
 		timeout.tv_sec = 10;
 		timeout.tv_usec = 0;
 		memset(client_message, 0, 200);
+		memset(buffer, 0, 200);
 		FD_ZERO(&fd);
 		FD_SET(sock, &fd);
+		FD_SET(socket_desc, &fd);
 		if (!select(sock + 1, &fd, NULL, &fd, &timeout))
 		{
 			printf("Timeoueted\n");
 			continue;
 		}
 		printf("Temps restant: %ld %ld", timeout.tv_sec, timeout.tv_usec);
+		if (FD_ISSET(sock, 
 		recv(sock, client_message, 200, 0);
 		if (!strcmp(client_message, "up"))
 		{
@@ -67,10 +88,12 @@ int main(void)
 		}
 		else if (!strcmp(client_message, "see"))
 		{
-			char buffer[200];
 			sprintf(buffer, "Current pos: %d %d\n", playerx, playery);
 			send(sock, buffer, strlen(buffer), 0);
-
+		}
+		else
+		{
+			send(sock, "invalid command", 7 + 1 + 7, 0);
 		}
 		printf("Client sent: %s\n", client_message);
 
