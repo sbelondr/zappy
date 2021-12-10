d9887f05b6d3a5efed6f817139358c8e18b5e8b6
jayache <jayache@student.42.fr>
2021-12-10 10:54:42 +0100

    small additions

--- server/goodclient.rb
+++ server/goodclient.rb
@@ -6,6 +6,7 @@ if ARGV.empty?
 	puts "You must pass the team name as first argument"
 end
 
+array = ['voir', 'avance', 'droite', 'gauche', 'prendre FOOD', 'poser FOOD', 'fork']
 s = TCPSocket.new 'localhost', 8080
 
 puts s.recv(99)
@@ -13,8 +14,6 @@ s.puts ARGV[0]
 puts s.recv(99)
 s.puts 'voir'
 loop do
-  s.puts 'avance'
-  puts s.recv(99)
-  s.puts 'voir'
+  s.puts array.sample
   puts s.recv(99)
 end
