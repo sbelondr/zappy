a9f926e17917217a313eb2c03a5ca22fef77432a
jayache <jayache@student.42.fr>
2022-02-06 15:29:03 +0100

    geger

--- server/srcs/listen.c
+++ server/srcs/listen.c
@@ -6,7 +6,7 @@
 /*   By: sbelondr <sbelondr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 20:58:11 by sbelondr          #+#    #+#             */
-/*   Updated: 2022/02/05 12:53:40 by jayache          ###   ########.fr       */
+/*   Updated: 2022/02/06 14:14:42 by jayache          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,17 @@
 void ft_listen_srv(t_srv *srv, fd_set *readfds)
 {
 	char buff[BUFF_READ];
-	int sd;
 	int i;
 	int	valread;
 
 	i = -1;
 	while (++i < srv->param->team_hard_limit * 2)
 	{
-		sd = srv->client_sck[i];
-		if (FD_ISSET(sd, readfds))
+		if (FD_ISSET(srv->client_sck[i], readfds))
 		{
 			bzero(buff, BUFF_READ);
-			if ((valread = read(sd, buff, BUFF_READ)) == 0)
-				ft_client_exit(srv, sd, i);
+			if ((valread = read(srv->client_sck[i], buff, BUFF_READ)) == 0)
+				ft_client_exit(srv, i);
 			else
 				ft_client_send_data(srv, buff, valread, i); //Genre ça ça veut dire que tu envoies des données du serveur au client non ????
 		}
