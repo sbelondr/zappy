67b4c17afcc09857c144ef7d133eb06f00696524
jayache <jayache@student.42.fr>
2022-02-11 11:13:59 +0100

    Tweaked blocks and updated readme

--- README.md
+++ README.md
@@ -1,23 +1,4 @@
 # TODO:
-## Sockets
-- After disconnecting a lot of clients, the server start acting weird and stop listening to new clients (They can connect, but cannot send/receive much)
-- The server doesn't seem to detect *every* disconnection
-- To reproduce:
-1. Start server
-2. Start client "Livin and forkin"
-3. Wait till about a hundred players are on the server
-4. Kill the client (CTRL+C), the server will not disconnect all clients, despite them not existing anymore
-5. Try to connect any client to the server, the server will quickly stop answering
-6. Disconnect the new client, the server won't disconnect it
-7. Connect a new client, the server will answer once or twice and then stop.[^1]
-8. Disconnect it, the server will properly register it
-9. Go back to step 5.
-
-UPDATE:
-It looks like now, even without manual intervention the serveur will start "forgetting" clients. This *may* just be "Livin and Forking" acting up.
-This happens at around ~400 clients. The serveur will only answer correctly to the ~ 15 first clients, and will not talk to the others one besides registering them.
-This may be related to the serveur disconnecting client from time to time.
-
 ## Performance
 - Disconnecting clients is relatively slow. 
 - Vision command is heavily optimizable
--- graphique/Script/block.gd
+++ graphique/Script/block.gd
@@ -1,24 +1,41 @@
 extends Spatial
 
-enum GEM {
-	BLUE,
-	YELLOW,
-	RED,
-	GREEN,
-	ORANGE,
-	PINK,
-	PURPLE
-}
-var gems = Dictionary()
+var item_names := ["Food", "Linemate", "Deraumere", "Sibur", "Mendiane", "Phiras", "Thystame"]
+var block_content := [0, 0, 0, 0, 0, 0, 0]
 
+# calc scale for the items
+func calc_scale(quantity: int):
+	var scale := 1.0
+	if quantity > 9:
+		quantity = 9
+	if quantity < 0:
+		quantity = 0
+	scale = quantity / 100.0
+	scale *= 2
+	return Vector3(scale, scale, scale)
+	
 
-#var gems := [[0, null], [0, null], [0, null], [0, null], [0, null], [0, null], [0, null]]
+#Add *one* item
+#item_id is based off the server's list
+func add_item(item_id: int):
+	var item : Spatial = get_node("Materials/%s" % item_names[item_id])
+	item.scale += Vector3.ONE * 0.01
+	item.scale = calc_scale(block_content[item_id])
 
-# Called when the node enters the scene tree for the first time.
+#Remove *one* item
+#item_id is based off the server's list
+func remove_item(item_id: int):
+	var item : Spatial = get_node("Materials/%s" % item_names[item_id])
+	block_content[item_id] -= 1
+	item.scale = calc_scale(block_content[item_id])
+
+#Change the whole inventory, useful during setup / desync
+#The inventory is represented with an array of size 7
+func set_inventory(inventory: Array):
+	block_content = inventory
+	for item_id in len(block_content):
+		get_node("Materials/%s" % item_names[item_id]).scale = calc_scale(block_content[item_id])
+	
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	for child in get_node("Materials").children():
+		child.visible = false
