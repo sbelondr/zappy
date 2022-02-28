73bea5c9821c8dd2026019e7521eae5c1e674af5
potato <superpotager@gmail.com>
2022-02-28 13:42:49 +0100

    Added tests that do not work

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -6,12 +6,11 @@ def connect(team)
   s.gets
   s.puts team
   s.gets
-  s.gets
   s
 end
 RSpec.describe 'Moving to a target direction' do
   before(:each) do
-    @client = connect 'TOTO'
+    @client = Client::Trantorien.new 'TOTO'
     @tester = connect 'TESTER'
   end
   after(:each) do
@@ -19,6 +18,32 @@ RSpec.describe 'Moving to a target direction' do
   end
 
   context 'in a straight line' do
+    it 'goes straight in front' do
+      @tester.puts 'set ppo #0 3 3 1'
+      @tester.gets
+      @client.move_towards [0, 2]
+      @tester.puts 'get ppo #0'
+      player_pos = @tester.gets
+      expect(player_pos).to eq("ppo #0 3 1 1\n")
+      @tester.puts 'set ppo #0 3 3 3'
+      @tester.gets
+      @client.move_towards [0, 2]
+      @tester.puts 'get ppo #0'
+      player_pos = @tester.gets
+      expect(player_pos).to eq("ppo #0 3 3 3\n")
+  end
+    it 'goes to the left' do
+      @tester.puts 'set ppo #0 3 3 1'
+      @tester.gets
+      @client.move_towards [2, 0]
+      @tester.puts 'get ppo #0'
+      player_pos = @tester.gets
+      expect(player_pos).to eq("ppo #0 5 3 1\n")
+      @client.move_towards [1, 0]
+      @tester.puts 'get ppo #0'
+      player_pos = @tester.gets
+      expect(player_pos).to eq("ppo #0 6 3 1\n")
+  end
 
   end
   context 'in diagonal' do
